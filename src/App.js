0953d118650b9462f84f4c9a14b952422b0a8fd0
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-10-15 16:36:57 -0400

    Form is working, sam change size and souse and keep them in the state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,67 @@ import React, { useState, useEffect } from 'react'
 import HomePage from './components/HomePage.js'
 import Form from './components/Form.js'
 import PizzaPost from './components/Pizza.js'
-
+import axios from 'axios'
 
 import { Route, Link } from 'react-router-dom'
+// name: string,
+//     size: string,
+//     topping1: bool,
+//     topping2: bool,
+//     special: string,
 
-
+const initialFormValues = {
+    name: '',
+    size: '',
+    source: '',
+    souce1: false,
+    souce2: false,
+    special: '',
+}
+const initialFormErrors = {
+  name: '',
+  size: '',
+  souce: '',
+  special: '',
+}
 
 const App = () => {
+
+const [formValues, setFormValues] =useState([initialFormValues]);
+const [formErrors, setFormErrors] = useState(initialFormErrors);
+const [confirmation, setConfirmation] = useState(initialFormValues);
+const [disabled, setDisabled] = useState();
+
+const change = (name, value) =>{
+  setFormValues({ ...formValues, [name]:value })
+}
+const setNewPizza = (newOrder) =>
+{
+    setConfirmation(newOrder);
+    setFormValues(initialFormValues);
+};
+
+ const submit = () =>
+    {
+        const newOrder = {
+            name: formValues.name.trim(),
+            size: formValues.size,
+            souce: ['souce1', 'souce2'].filter(top => !!formValues[top]),
+            special: formValues.special.trim()
+        };
+
+        setNewPizza(newOrder);
+    };
+
+useEffect(() => {
+  axios.get('https://reqres.in/api/orders')
+    .then(res => {
+     console.log(res.data.data)
+     
+    }).catch(err => console.error(err));
+}, [])
+
+
   return (
     <div className="App">
     <header>
@@ -23,7 +77,7 @@ const App = () => {
     <PizzaPost />
     </Route> */}
     <Route path="/pizza">
-      <Form />
+      <Form change={change} submit={submit} values={formValues} />
     </Route>
     <Route exact path="/"> 
       <HomePage />
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,8 +2,64 @@ import React from 'react';
 
 
 const Form = (props) => {
-   return (
-       <div>Form page</div>
+    const {
+        submit,
+        values,
+        change
+
+      } = props
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+      }
+    const onChange = evt => {
+    
+    const { name, value, checked, type } = evt.target;
+    const valueToUse = type === 'checkbox' ? checked : value;
+    change(name, valueToUse);
+      }
+    return (
+        <div>
+            <form onSubmit={props.submit} id='pizza-form'>
+                <h2>Build Your Own Pizza</h2>
+                <label> Choice Of Size
+                <select
+                    onChange={onChange}
+                    value={values.size}
+                    name='size'
+                >
+                    <option value=''>- Select -</option>
+                    <option value='small'>Small</option>
+                    <option value='medium'>Madium</option>
+                    <option value='lange'>Large</option>
+                
+                </select>
+                </label>
+
+                <h2>Choice of Souse</h2> 
+                 <label> Souce 1  
+                <input 
+                    type="radio"
+                    name="souce"
+                    value="souce1"
+                    onChange={onChange}
+                    checked={values.souce === 'souse1'}
+                />
+                </label>
+
+                <label> Souce 2
+                <input
+                    type="radio"
+                    name="souce"
+                    value="souce2"
+                    onChange={onChange}
+                    checked={values.souce === 'souce2'}
+                />   
+                </label>
+
+            </form>
+        </div>
    )
     
 }
